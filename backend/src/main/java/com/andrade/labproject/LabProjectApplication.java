989a21ca79c6bddd9fa9320986883b92093002e3
ribastudio <ribastudio@gmail.com>
2024-03-11 20:43:32 -0300

    add: initial commit

---
+++ backend/src/main/java/com/andrade/labproject/LabProjectApplication.java
@@ -0,0 +1,13 @@
+package com.andrade.labproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabProjectApplication.class, args);
+	}
+
+}
